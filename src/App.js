25cdf8134ef41336d2bbb241ee290707ca872cc6
unknown <C:\Users\NF1\AppData\Roaming\The Bat!>
2025-05-08 04:13:37 +0300

    adds block info

--- src/App.js
+++ src/App.js
@@ -21,16 +21,75 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockHash, setBlockHash] = useState();
+  const [transactions, setTransactions] = useState([]);
+  const [transcationInfo, setTransactionInfo] = useState([]);
 
   useEffect(() => {
     async function getBlockNumber() {
       setBlockNumber(await alchemy.core.getBlockNumber());
     }
 
-    getBlockNumber();
-  });
+    async function getBlockHash() {
+      const block = await alchemy.core.getBlock(blockNumber);
+      const hash = block.hash;
+      setBlockHash(hash);
+    }
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+    async function getTransactions() {
+      const block = await alchemy.core.getBlock(blockNumber);
+      const transactions = block.transactions;
+      setTransactions(transactions);
+    }
+
+    async function getTransactionInfo() {
+      const transaction = await alchemy.core.getTransactionReceipt(transactions[0]);
+      setTransactionInfo(transaction);
+    }
+
+    getBlockNumber();
+    getBlockHash();
+    getTransactions();
+    getTransactionInfo();
+  }, []);
+
+  return <div className="App">
+    Block Number: {blockNumber}
+    <br/>
+    Block Hash: {blockHash}
+    <br/>
+    Transactions: {transactions.length}
+    <br/>
+    First transaction information:
+    <br/>
+    from: {transcationInfo.from}
+    <br/>
+    to: {transcationInfo.to}
+    <br/>
+    gasUsed: {transcationInfo.gasUsed.toString()}
+    <br/>
+    blockHash: {transcationInfo.blockHash}
+    <br/>
+    blockNumber: {transcationInfo.blockNumber}
+    <br/>
+    transactionHash: {transcationInfo.transactionHash}
+    <br/>
+    transactionIndex: {transcationInfo.transactionIndex}
+    <br/>
+    contractAddress: {transcationInfo.contractAddress}
+    <br/>
+    cumulativeGasUsed: {transcationInfo.cumulativeGasUsed.toString()}
+    <br/>
+    effectiveGasPrice: {transcationInfo.effectiveGasPrice.toString()}
+    <br/>
+    logs: {transcationInfo.logs.length}
+    <br/>
+    status: {transcationInfo.status.toString()}
+    <br/>
+    type: {transcationInfo.type.toString()}
+    <br/>
+    confirmations: {transcationInfo.confirmations.toString()}
+  </div>;
 }
 
 export default App;
